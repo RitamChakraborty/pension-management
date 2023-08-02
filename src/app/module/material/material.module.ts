3891376944f75ab0029c61b4b3417569d2192fa6
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-02 17:43:14 +0530

    Update angular material version to version 15

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
+import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
+import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
+import {MatLegacySnackBarModule as MatSnackBarModule} from "@angular/material/legacy-snack-bar";
 import {MatIconModule} from "@angular/material/icon";
-import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatLegacyProgressSpinnerModule as MatProgressSpinnerModule} from "@angular/material/legacy-progress-spinner";
 
 const MaterialComponents = [
   MatButtonModule,
