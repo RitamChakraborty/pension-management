5a3efaf78ff0d95163b3d754daccc3b9fab8bb4f
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-02 19:13:14 +0530

    Fix imports

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
-import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
-import {MatLegacyFormFieldModule as MatFormFieldModule} from "@angular/material/legacy-form-field";
-import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
-import {MatLegacySnackBarModule as MatSnackBarModule} from "@angular/material/legacy-snack-bar";
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from "@angular/material/icon";
-import {MatLegacyProgressSpinnerModule as MatProgressSpinnerModule} from "@angular/material/legacy-progress-spinner";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 const MaterialComponents = [
   MatButtonModule,
