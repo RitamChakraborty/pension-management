bc9c38591fa464dff842c45083da0af282dea323
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-03 15:28:07 +0530

    Add auth guard to home compoent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {LoginComponent} from "./pages/login/login.component";
+import {AuthGuardGuard} from "./guard/auth-guard/auth-guard.guard";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    canActivate: [AuthGuardGuard],
     data: {
       animation: 'home',
     }
--- src/app/guard/auth-guard/auth-guard.guard.ts
+++ src/app/guard/auth-guard/auth-guard.guard.ts
@@ -1,30 +1,30 @@
 import {Injectable} from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {map, Observable} from 'rxjs';
 import {AuthenticateService} from "../../service/authenticate/authenticate.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class AuthGuardGuard  {
-  constructor(
-    private authenticationService: AuthenticateService,
-  ) {
-  }
+export class AuthGuardGuard {
+    constructor(
+        private authenticationService: AuthenticateService,
+    ) {
+    }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authenticationService.authenticate()
-      .pipe(
-        map(value => {
-          if (!value) {
-            this.authenticationService.logout();
-          }
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        return this.authenticationService.authenticate()
+            .pipe(
+                map(value => {
+                    if (!value) {
+                        this.authenticationService.logout();
+                    }
 
-          return value;
-        })
-      );
-  }
+                    return value;
+                })
+            );
+    }
 }
