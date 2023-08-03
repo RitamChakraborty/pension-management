d2463839b6ad8bea0edf464b590b112ad700c8d1
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-08-03 09:08:04 +0530

    Fix import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
 import {LoginComponent} from "./pages/login/login.component";
-import {AuthGuardGuard} from "./guard/auth-guard/auth-guard.guard";
 
 const routes: Routes = [
   {
